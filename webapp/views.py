e83904dbcb45100bf4dc01919deeff8bf448857a
mateo <mateopolicichio@gmail.com>
2023-07-26 19:05:26 -0300

    Inicializacion de proyecto

---
+++ webapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'webapp/index.html')
+
+def blog(request):
+    return render(request, 'webapp/blog.html')
+
+def blog_details(request):
+    return render(request, 'webapp/blog-details.html')
+
+def services_details(request):
+    return render(request, 'webapp/services-details.html')
+
+def portfolio_details(request):
+    return render(request, 'webapp/portfolio-details.html')
